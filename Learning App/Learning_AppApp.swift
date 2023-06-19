c1c64b203d37bda4757069286b4c727bcb18358f
Siddronio <jhonsidr@gmail.com>
2023-06-19 17:32:37 -0300

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Jhonatan Sidrônio on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
